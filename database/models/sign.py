7f5da1bded9794f972008c336494bbaa9290ebde
方OK\44576 <445764559@qq.com>
2019-07-15 23:44:16 +0800

    update account

--- database/models/sign.py
+++ database/models/sign.py
@@ -9,12 +9,12 @@ from api.utils.secret import render_password
 class Sign(DB.Model):
     __tablename__ = 'flag_sign'
     # 此处缺少目标完成时间算出最大休假时间及最小休假时间
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)  # 签到id
-    uid = Column(Integer, primary_key=True, nullable=False)  # 用户id
-    target_id = Column(Integer, primary_key=True, nullable=False) # 目标id
-    target_name = Column(VARCHAR(255), nullable=False)# 目标标题
-    number_of_days = Column(Integer, nullable=False)  # 目标完成天数
-    create_time = Column(DateTime)  # 签到时间
-    img = Column(DB.TEXT) # 图片 各个图片以逗号隔开
-    status = Column(TINYINT, default=1)  # 目标状态 1 正常签到, 2 休假
-    content = Column(VARCHAR(255))  # 签到内容
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True, nullable=False)  # 签到id
+    uid = DB.Column(DB.Integer, primary_key=True, nullable=False)  # 用户id
+    target_id = DB.Column(DB.Integer, primary_key=True, nullable=False) # 目标id
+    target_name = DB.Column(DB.VARCHAR(255), nullable=False)# 目标标题
+    number_of_days = DB.Column(DB.Integer, nullable=False)  # 目标完成天数
+    create_time = DB.Column(DB.DateTime)  # 签到时间
+    img = DB.Column(DB.TEXT) # 图片 各个图片以逗号隔开
+    status = DB.Column(DB.TINYINT, default=1)  # 目标状态 1 正常签到, 2 休假
+    content = DB.Column(DB.VARCHAR(255))  # 签到内容
--- database/models/target.py
+++ database/models/target.py
@@ -9,21 +9,21 @@ from api.utils.secret import render_password
 class Target_Info(DB.Model):
     __tablename__ = 'flag_target_info'
     # 此处缺少目标完成时间算出最大休假时间及最小休假时间
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)  # 目标id
-    uid = Column(Integer, primary_key=True, nullable=False)  # 用户id
-    target_name = Column(VARCHAR(255), nullable=False)# 目标标题
-    number_of_days = Column(Integer, nullable=False)  # 目标完成天数
-    day_off = Column(VARCHAR(255), nullable=False)  # 休假时间
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True, nullable=False)  # 目标id
+    uid = DB.Column(DB.Integer, primary_key=True, nullable=False)  # 用户id
+    target_name = DB.Column(DB.VARCHAR(255), nullable=False)# 目标标题
+    number_of_days = DB.Column(DB.Integer, nullable=False)  # 目标完成天数
+    day_off = DB.Column(DB.VARCHAR(255), nullable=False)  # 休假时间
     # sign_time = Column(DateTime, nullable=False)# 签到时间
-    annotation = Column(VARCHAR(255))  # 备注
-    challenge_gold = Column(INT, default=0)  # 挑战金额（积分）  最低金额 100分，积分：100
-    insist_day = Column(INT)# 当前坚持天数
-    privacy = Column(INT)  # 是否公开,1公开,2隐私
-    create_time = Column(DateTime)  # 创建时间
-    modified_time = Column(DateTime)  # 修改时间
-    reminder_time = Column(DB.VARCHAR(30))  # 提醒时间
-    pay_type = Column(VARCHAR(255))  # 支付类型 默认1：支付宝 2：微信 3：用户余额，4：积分
-    status = Column(TINYINT, default=1)  # 目标状态 0 正在进行,1 成功, 2 失败
+    annotation = DB.Column(DB.VARCHAR(255))  # 备注
+    challenge_gold = DB.Column(DB.INT, default=0)  # 挑战金额（积分）  最低金额 100分，积分：100
+    insist_day = DB.Column(DB.INT)# 当前坚持天数
+    privacy = DB.Column(DB.INT)  # 是否公开,1公开,2隐私
+    create_time = DB.Column(DB.DateTime)  # 创建时间
+    modified_time = DB.Column(DB.DateTime)  # 修改时间
+    reminder_time = DB.Column(DB.VARCHAR(30))  # 提醒时间
+    pay_type = DB.Column(DB.VARCHAR(255))  # 支付类型 默认1：支付宝 2：微信 3：用户余额，4：积分
+    status = DB.Column(DB.TINYINT, default=1)  # 目标状态 0 正在进行,1 成功, 2 失败
     gold_type = DB.Column(DB.Integer, default=1) #挑战金额类型默认1：金额   2：积分
     start_time = DB.Column(DB.DateTime) # 开始时间
     end_time = DB.Column(DB.DateTime) # 结束时间
